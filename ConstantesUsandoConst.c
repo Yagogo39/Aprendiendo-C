b21a87a9bd0ba1d7f151082305ac9ae939b42abf
Yagogo39 <118009609+Yagogo39@users.noreply.github.com>
2025-02-13 19:28:08 -0600

    Otra forma de definir constantes

---
+++ ConstantesUsandoConst.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	const int val =10;
+	const float floatVal = 4.5;
+	const char charVal = 'G';
+	const char stringVal[10] = "ABC";
+
+	printf("Constante entera: %d\n", val);
+	printf("Constante de punto flotante: %.lf\n", floatVal);
+	printf("Constante caracter: %c\n", charVal);
+	printf("Cadena Constante: %s\n", stringVal);
+
+	return 0;
+}
