a901d6f836080efd6d3b9ca4430e84e8afb226c2
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-04-22 22:15:38 -0600

    OperadoresLogicos.c

--- OperadoresLogicos.c
+++ OperadoresLogicos.c
@@ -4,7 +4,7 @@ int main(void){
 	int p=10, q=0, and, or, not;
 	and=p&&q; //El resultado es 0
 	or=p||q; //El resultado es 1
-	not=!p; //El resultado es 0
+	not=!p; //El resultado es 00
 
 	printf("%d && %d = %d\n", p,q, and);
 	printf("%d || %d = %d\n", p,q, or);
